f295a83bedceb9f86fe053d4158559338ca94c4d
nikhil <nikh.sant123@gmail.com>
2026-03-24 12:35:50 +0530

    removing keys

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBnh-2V18d36gjjbmirfRlP5-_HcoR0Clc")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-
-response = model.generate_content("Say hello")
-
-print(response.text)
